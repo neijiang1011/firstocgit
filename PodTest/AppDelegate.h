81cb5081c9cea7e3b7ccf1563f0ce9b28c9ee176
yetao ye <yetaoye@yetaodemacbook-pro.local>
2018-07-26 22:34:32 +0800

    Initial Commit

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by yetao ye on 2018/7/26.
+//  Copyright © 2018年 com.yt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by yetao ye on 2018/7/26.
+//  Copyright © 2018年 com.yt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
